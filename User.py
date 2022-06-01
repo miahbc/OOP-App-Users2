d6970d51acf9824d4e98b4c48846e5ac56db61b5
Miah Clay <miahb88@gmail.com>
2022-06-01 15:10:38 -0400

    started repo

--- User.py
+++ User.py
@@ -1,1 +1,14 @@
 # your improved User class goes here
+
+class Users:
+    def __init__(self, name, email, dln):
+        self.name = name
+        self.email = email
+        self.dln = dln
+
+
+tony = Users('Anthony','abc@gmail.com','12345')
+ana = Users('Viviana', 'vjc@gmail.com', '67890')
+miah = Users('Miah', 'mac@gmail.com', '98765')
+
+print(tony.dln)
